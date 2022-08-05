bae7d176ace0dfc105ca78b3307c84ade5e31cce
Ayodeji Okulaja <koats14@gmail.com>
2022-08-05 23:40:07 +0100

    last commit

--- Day1 Assignment.js
+++ Day1 Assignment.js
@@ -5,7 +5,43 @@
 // Welcome to 30DaysOfJavaScript
 
 //3
-// comments can make code readable,
-// easy to reuse and informative
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
 
 //4
+
+//5
+
+//6
+let firstVar;
+let secondVar;
+let thirdVar;
+let fourthVar;
+
+//7
+let varOne = "first variable";
+let varTwo = "second variable";
+let varThree = "third variable";
+let varFour = "fourth variable";
+
+//8
+let firstName = "Ayodeji";
+let lastName = "Okulaja";
+let maritalStatus = "single";
+let country = "Nigeria";
+let age = "96";
+
+//9
+let firstName1 = "Ayodeji",
+  lastName2 = "Okulaja",
+  maritalStatus2 = "single",
+  country2 = "Nigeria",
+  age2 = "96";
+
+//10
+let myAge = "96";
+let yourAge = "69";
+console.log(myAge);
+console.log(yourAge);
